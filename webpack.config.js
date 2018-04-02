7f9c0dd87afea64b2e4a5ed88fa6190d46bff5bc
TravisT <travis.don.tillotson@gmail.com>
2018-04-02 16:39:32 -0700

    App Config Set up package.json with expected dependencies for react/redux set up webpack config files Set up Ruby Gems

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/tiptoptomes.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};

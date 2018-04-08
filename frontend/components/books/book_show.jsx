3e7f402f25acb84ecbe89b5cbf7e7554d41b04ab
TravisT <travis.don.tillotson@gmail.com>
2018-04-07 20:25:01 -0700

    Build Show Page

---
+++ frontend/components/books/book_show.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class BookShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchBook(this.props.params.match.bookId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.params.bookId !== nextProps.match.params.bookId) {
+      this.props.fetchBook(nextProps.match.params.bookId);
+    }
+  }
+
+  render() {
+    const { book } = this.props;
+
+    return (
+      <article>
+        <img className={'book_image'} src={`${book.img_url}`} alt={`Book Cover`} />
+        <h1>
+          Title: {book.title}
+        </h1>
+        <h3>
+          Author: {book.author}
+        </h3>
+        <h3>
+          Description: {book.description}
+        </h3>
+        <h3>
+          Published: {book.published}
+        </h3>
+        <h3>
+          ISBN: {book.ISBN}
+        </h3>
+      </article>
+    );
+  }
+}
+
+export default BookShow;
---
+++ frontend/components/books/book_show_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import BookShow from './book_show';
+import { fetchBook } from '../../actions/book_actions';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  book: state.entities.books[ownProps.match.params.bookId],
+  loading: state.ui.loading.bookLoading
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchBook: (bookId) => dispatch(fetchBook(bookId))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookShow);

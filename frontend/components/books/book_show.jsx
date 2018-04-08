5a630452b424694c33d746d7a601a326b2f80813
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-04-08 16:08:29 -0700

    Style Show Page

--- frontend/components/books/book_show.jsx
+++ frontend/components/books/book_show.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoadingIcon from '../shared/loading_icon';
+import ReviewIndexContainer from 'review_index_container';
 
 class BookShow extends React.Component {
   componentDidMount() {
@@ -20,35 +21,42 @@ class BookShow extends React.Component {
     if (!book) { return null; }
 
     return (
-      <article className='book_show'>
-        <section className='book_info'>
+      <div className='book_show_container'>
+        <article className='book_show'>
+          <section className='book_info'>
+            <section className='book_image_col'>
+              <img className='book_show_image'
+                src={`${book.img_url}`}
+                alt={`Book Cover`} />
+            </section>
 
-          <img className='book_show_image'
-            src={`${book.img_url}`}
-            alt={`Book Cover`} />
+            <section className='book_info_detail'>
+              <h1 className='book_title'>
+                {book.title}
+              </h1>
 
-          <h1 className='book_title'>
-            Title: {book.title}
-          </h1>
+              <h3 className='book_author'>
+                <span>by</span> {book.author}
+              </h3>
 
-          <h3>
-            Author: {book.author}
-          </h3>
+              <h3 className='book_description'>
+                {book.description}
+              </h3>
 
-          <h3>
-            Description: {book.description}
-          </h3>
+              <h3 className='book_published'>
+                Published: {book.published}
+              </h3>
 
-          <h3>
-            Published: {book.published}
-          </h3>
+              <h3 className='book_ISBN'>
+                ISBN: {book.ISBN}
+              </h3>
 
-          <h3>
-            ISBN: {book.ISBN}
-          </h3>
+            </section>
+          </section>
 
-        </section>
-      </article>
+          <REVIEWS>
+        </article>
+      </div>
     );
   }
 }
--- frontend/components/reviews/review_index_container.js
+++ frontend/components/reviews/review_index_container.js
@@ -2,11 +2,11 @@ import { connect } from 'redux';
 import ReviewIndex from './review_index';
 
 const mapStateToProps = (state, ownProps) => ({
-
+  reviews: state.books[ownProps.match.params.bookId].reviews
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  
 });
 
 export default connect(
